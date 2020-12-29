570c415084aac11ba59105bfb02f3f22a14ef36e
Antonin Maystre <amaystre@iutbayonne.univ-pau.fr>
2020-12-29 23:02:23 +0100

    tp3

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Entreprise;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //$faker = Faker\Factory::create('fr_FR');
+
+        $stage1=new Entreprise();
+        $stage1->setNom("Safran");
+        $stage1->setAdresse("16 rue des pompiers");
+        $stage1->setSite("www.safran.com");
+
+        $manager->persist($stage1);
 
         $manager->flush();
     }
