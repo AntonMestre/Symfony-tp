c772eb6ae8aec041166b4d15d45b443d4ee33900
Antonin Maystre <amaystre@iutbayonne.univ-pau.fr>
2021-01-08 18:44:56 +0100

    Complétion des commentaires*

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,21 +9,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProStageController extends AbstractController
 {
 
+    /*
+    * Controlleur de la page d'accueil (nommée index ci-dessous)
+    */
     public function index(): Response
     {
         return $this->render('pro_stage/index.html.twig');
     }
 
+    /*
+    * Controlleur de la page des stages classés par entreprise
+    */
     public function entreprise(): Response
     {
         return $this->render('pro_stage/entreprise.html.twig');
     }
 
+    /*
+    * Controlleur de la page des stages classés par formation
+    */
     public function formation(): Response
     {
         return $this->render('pro_stage/formation.html.twig');
     }
 
+    /*
+    * Controlleur de la page permettant de voir les détails d'un stage
+    */
     public function stage($id): Response
     {
         return $this->render('pro_stage/stage.html.twig', [
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,24 +19,30 @@ class AppFixtures extends Fixture
         // Génération des données de test pour l'entité Formation  //
         /////////////////////////////////////////////////////////////
 
+        // Création de la formation : DUT Informatique avec sa description
         $dutInfo = new Formation();
         $dutInfo -> setTitre("Dut informatique");
         $dutInfo -> setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
 
+        // Création de la formation : Ecole d'ingenieur avec sa description
         $ecoldeInge = new Formation();
         $ecoldeInge -> setTitre("Ecole d'ingénieur informatique");
         $ecoldeInge -> setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
 
+        // Création de la formation : Licence 3 informatique avec sa description
         $licence = new Formation();
         $licence -> setTitre("Licence 3 informatique");
         $licence -> setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
 
+        // Création de la formation : BTS SIO avec sa description
         $bts = new Formation();
         $bts -> setTitre("BTS SIO");
         $bts -> setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
 
+        // Création d'un tableau pour faciliter l'envoie en base de données
         $tableauFormations = array($dutInfo,$ecoldeInge,$licence,$bts);
 
+        // Boucle pour créer les formations en base de données
         foreach ($tableauFormations as $formation) {
             $manager->persist($formation);
         }
@@ -47,7 +53,6 @@ class AppFixtures extends Fixture
 
         // Nombre d'entreprise qui seront générées
         $nbEntreprise=15;
-
         for($i=0; $i <= $nbEntreprise; $i++){
 
           // Création d'une entreprise
@@ -65,23 +70,29 @@ class AppFixtures extends Fixture
           // Enregistrement de l'entreprise créer
           $manager->persist($entreprise);
 
+          // Création des stages (entre 0 et 3 pour chaque entreprise)
           for($o=0; $o < $faker->numberBetween($min = 0, $max = 3);$o++){
 
+            // Création du stage avec son titre, l'activité, l'email et l'entreprise rattaché
             $stage=new Stage();
             $stage->setTitre($faker->realText($maxNbChars = 50, $indexSize = 2));
             $stage->setActivite($faker->realText($maxNbChars = 50, $indexSize = 2));
             $stage->setEmail($faker->companyEmail());
             $stage->setEntreprise($entreprise);
 
-            $nbrFormation = $faker->numberBetween($min = 0, $max = 3);
+            // Nombre de formation rattaché au stage (entre 1 et 3 maximum)
+            $nbrFormation = $faker->numberBetween($min = 1, $max = 3);
+            for($m=0;$m<$nbrFormation;$m++){
 
-            for($m=0;$m<$nbEntreprise;$m++){
-
+              // Tirage d'un chiffre pour choisir parmis les formations du tableau laquelle sera choisie
               $laFormation = $faker->numberBetween($min = 0, $max = 3);
+
+              // Ajout de la formtation au stage (et inversement)
               $stage->addFormation($tableauFormations[$laFormation]);
               $tableauFormations[$laFormation]->addStage($stage);
             }
 
+            // Application sur la BD
             $manager->persist($stage);
             $manager->persist($tableauFormations[$laFormation]);
           }
