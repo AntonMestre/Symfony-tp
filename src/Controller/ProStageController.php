4ee01b9a0bbdac812f04d296c55c845fb504b07a
Antonin Maystre <amaystre@iutbayonne.univ-pau.fr>
2021-02-04 12:31:51 +0100

    ajout du formulaire pour les entreprises

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -11,6 +11,10 @@ use App\Entity\Formation;
 use App\Repository\StageRepository;
 use App\Repository\EntrepriseRepository;
 use App\Repository\FormationRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Persistence\EntityManagerInterface;
 
 class ProStageController extends AbstractController
 {
@@ -91,4 +95,38 @@ class ProStageController extends AbstractController
         ]);
     }
 
+
+    /*
+    * Controller de la page permettant d'ajouter une entreprise
+    */
+    public function ajouterEntreprise(Request $request,EntityManagerInterface $manager): Response
+    {
+      //Création d'une ressource vierge qui sera remplie par le formulaire
+       $entreprise = new Entreprise();
+
+       // Création du formulaire permettant de saisir une ressource
+       $formulaireEntreprise = $this->createFormBuilder($entreprise)
+       ->add('nom',TextType::class)
+       ->add('adresse',TextType::class)
+       ->add('site',UrlType::class)
+       ->getForm();
+
+       /* On demande au formulaire d'analyser la dernière requête Http. Si le tableau POST contenu
+      dans cette requête contient des variables titre, descriptif, etc. alors la méthode handleRequest()
+      récupère les valeurs de ces variables et les affecte à l'objet $ressource*/
+      $formulaireEntreprise->handleRequest($request);
+
+       if ($formulaireEntreprise->isSubmitted() )
+       {
+          // Enregistrer la ressource en base de donnéelse
+          $manager->persist($entreprise);
+          $manager->flush();
+
+          // Rediriger l'utilisateur vers la page d'accueil
+          return $this->redirectToRoute('accueil');
+       }
+
+       // Envoyer la formation récupérées à la vue chargée de les afficher
+        return $this->render('pro_stage/ajoutEntreprise.html.twig',['vueFormulaire' => $formulaireEntreprise->createView()]);
+    }
 }
